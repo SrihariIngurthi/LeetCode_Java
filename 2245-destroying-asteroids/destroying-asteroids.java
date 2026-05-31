8b02b87d112a3cee7d21c100799c7626a54ba372
SrihariIngurthi <ingurthihari1857@gmail.com>
2026-05-31 11:24:59 +0530

    Time: 26 ms (81.52%) | Memory: 107.7 MB (7.85%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+    Arrays.sort(asteroids);
+
+    long curr = mass;
+
+    for(int a : asteroids) {
+        if (curr < a) return false;
+        curr += a;
+    }        
+    return true;
+    }
+}
